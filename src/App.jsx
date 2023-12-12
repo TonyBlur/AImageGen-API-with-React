02ea6e9e68ee17c5325315f7b259d166eebcd4f8
Liu <zehao100@qq.com>
2023-12-12 15:40:52 +0800

    - add useEffect reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 import { DisplayImages } from "./Images";
 import ImageDownloader from "./ImagesDownload";
